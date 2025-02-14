6e66ec064a098ddc66ac09a47b955d5b73dc4337
JanLascala <lascala.jan@gmail.com>
2025-02-14 17:19:14 +0100

    Work

--- Palindromi/main.js
+++ Palindromi/main.js
@@ -1,12 +1,29 @@
 // ask name
-const userName = [prompt("write your name")]
+const userName = prompt("write your name")
 console.log(userName)
 
-
 //reverse name
-const reversedName = userName.reverse();
-console.log(reversedName)
+const reverse = userName.split('').reverse().join('');
+console.log(reverse)
 
 //check if same
+const sumnames = userName + reverse
+console.log(sumnames)
+
+
+function checkifpalindrome(sumnames) {
+    let result = ``
+    if (userName == reverse) {
+        result = `palindrome`
+        console.log("palindorme!")
+    } else {
+        result = `not palindrome`
+        console.log("not palindrome!")
+    }
+    return result
+}
+
+const palindorme = checkifpalindrome(sumnames)
+
 
 //result
