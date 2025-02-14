5f3f2fa42804d9142c7eaf67e00c1c7cbcc8337c
JanLascala <lascala.jan@gmail.com>
2025-02-14 16:05:01 +0100

    Add Palindormi

---
+++ Palindromi/main.js
@@ -0,0 +1,12 @@
+// ask name
+const userName = [prompt("write your name")]
+console.log(userName)
+
+
+//reverse name
+const reversedName = userName.reverse();
+console.log(reversedName)
+
+//check if same
+
+//result
