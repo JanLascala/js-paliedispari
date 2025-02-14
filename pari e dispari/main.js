da1da95d720792c39905ab8c4039d2f01cc5be00
JanLascala <lascala.jan@gmail.com>
2025-02-14 16:38:00 +0100

    Rearrange order

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,31 +1,25 @@
+
 console.log("it works")
 
-//ask number ask if even or odd
+//ask  user number ask if even or odd
 const userchoice = prompt("choose odd or even")
 console.log(userchoice)
+
 const userNumber = Number(prompt("write a number between 1-5"));
 console.log(userNumber + " " + " your number")
 
-//generate number
+//pc generate number
 
 const Pc_number = Math.ceil(Math.random() * 5);
 console.log(Pc_number + " " + "Pc Number")
+
 //add numberpc + user
 
 let sum = Pc_number + userNumber;
 console.log(sum)
 
+// this is my function 
 
-//check numb if odd or even 
-const result = oddoreven(sum)
-
-// declaring winner
-
-if (userchoice == result) {
-    console.log("uwin")
-} else {
-    console.log("pc wins")
-}
 
 function oddoreven(sum) {
     let sum_type = ``
@@ -39,3 +33,14 @@ function oddoreven(sum) {
     return sum_type
 }
 
+//check numb if odd or even 
+
+const result = oddoreven(sum)
+
+// declaring winner
+
+if (userchoice == result) {
+    console.log("u win")
+} else {
+    console.log("pc wins")
+}
