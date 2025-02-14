ed5a312d6cb9c58e1bf4c0700881193cfa0ba71d
JanLascala <lascala.jan@gmail.com>
2025-02-14 15:19:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("it works")
+
+//ask number 
+const userNumber = Number(prompt("write a number between 1-5"));
+console.log(userNumber)
+//generate number
+
+const Pc_number = Math.ceil(Math.random() * 5);
+//add numberpc + user
+
+
+
+
+//check numb if odd or even 
